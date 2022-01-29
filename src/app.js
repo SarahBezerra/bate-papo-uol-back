ed4d67b0a5ba9f9e1b72bcb8aafe8d2af37145cd
Sarah <sarahbezerra2013@gmail.com>
2022-01-29 15:32:27 -0300

    esqueleto das rotas

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+//dotenv
+
+const app = express();
+app.use(cors());
+
+app.post('/participants', (req, res) => {
+
+});
+
+
+app.get('/participants', (req, res) => {
+
+});
+
+
+app.post('/messages', (req, res) => {
+
+});
+
+
+app.get('/messages', (req, res) => {
+
+});
+
+
+app.post('/status', (req, res) => {
+
+});
+
+
+app.listen(5000);

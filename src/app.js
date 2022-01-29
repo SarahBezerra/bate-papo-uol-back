2d58b8c61da54c44788cf82114bbfa0d6fb73cd5
Sarah <sarahbezerra2013@gmail.com>
2022-01-29 18:16:27 -0300

    valida nome com JOI

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import joi from 'joi';
 //dotenv
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
+const nameSchema = joi.object({
+    name: joi.string().required()
+})
+
 app.post('/participants', (req, res) => {
+    const validation = nameSchema.validate(req.body);
+    if(validation.error){
+        res.status(422).send(validation.error.details[0].message);
+        return;
+    }
+
+    res.sendStatus(201)
 
 });
 

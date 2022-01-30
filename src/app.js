f01c803135921a815feaebdab856f0104595984e
Sarah <sarahbezerra2013@gmail.com>
2022-01-30 00:04:12 -0300

    valida mensagens enviadas

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { MongoClient } from 'mongodb';
 import cors from 'cors';
 import joi from 'joi';
+import dayjs from 'dayjs';
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,12 @@ dotenv.config();
 
 const nameSchema = joi.object({
     name: joi.string().required()
+})
+
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.valid('message', 'private_message').required()
 })
 
 
@@ -64,8 +71,33 @@ app.get('/participants', async (req, res) => {
 });
 
 
-app.post('/messages', (req, res) => {
+app.post('/messages', async (req, res) => {
+    try {
+        const mongoClient = new MongoClient(process.env.MONGO_URI);
+        await mongoClient.connect();
 
+        if(!await mongoClient.db("batepapouol").collection("participants").findOne({ name: req.headers.user })){
+            res.sendStatus(422);
+            mongoClient.close();
+            return
+        }
+
+        const validation = messageSchema.validate(req.body, { abortEarly: false });
+        if(validation.error){
+            res.status(422).send(validation.error.details.map(error => error.message));
+            mongoClient.close();
+            return;
+        }
+
+        const message = { ...req.body, from: req.headers.user, time: dayjs().format('HH:mm:ss') };
+        await mongoClient.db("batepapouol").collection("messages").insertOne(message);
+        
+        res.sendStatus(201);
+        mongoClient.close()
+    }
+    catch(error) {
+        res.status(500).send(error)
+    }
 });
 
 
